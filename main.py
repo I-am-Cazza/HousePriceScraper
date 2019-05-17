25f320a5e0410773ac5d4eec4d19cb3d1be49f07
Cameron Hughes <cameronhughes0@btinternet.com>
2019-05-17 20:46:52 +0100

    Add support for Zoopla

--- main.py
+++ main.py
@@ -3,27 +3,40 @@ from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
 
+
+# Returns the average house price in London, formatted as "£00,000.00"
 def main():
-    print(get_average_price())
+    print("£" + "{:,.2f}".format(get_average_price()))
 
+
+# Finds the average price of houses in London according to the supported websites
 def get_average_price():
-    prices = list()
-    page = get_page("https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%5E87490&maxBedrooms=4&minBedrooms=1&sortType=6&includeSSTC=false")
-    prices = get_rightmove_prices(page)
+    page = get_page("https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%5E87490&sortType=6&propertyTypes=detached%2Csemi-detached%2Cterraced&primaryDisplayPropertyType=houses&includeSSTC=false")
+    prices = get_prices(page, "rightmove")
+    page = get_page("https://www.zoopla.co.uk/for-sale/houses/london/?page_size=100&q=London&radius=0&results_sort=newest_listings&search_source=refine")
+    prices.extend(get_prices(page, "zoopla"))
     average = sum(prices)/len(prices)
-    return "£" + "{:,.2f}".format(average)
+    return average
 
 
-def get_rightmove_prices(html):
+# Returns a list of all the prices found at the given site
+def get_prices(html, site_name):
     prices = list()
-    for div in html.find_all("div"):
-        if "class" in div.attrs and "propertyCard-priceValue" in div["class"]:
-            price = convert_price(str(div.string))
+    if site_name == "rightmove":
+        tag_type = "div"
+        class_name = "propertyCard-priceValue"
+    else:
+        tag_type = "a"
+        class_name = "listing-results-price"
+    for tag in html.find_all(tag_type):
+        if "class" in tag.attrs and class_name in tag["class"]:
+            price = convert_price(str(tag.string))
             if price is not None:
                 prices.append(price)
     return prices
 
 
+# Converts the string price in the format "£00,000,000.00" to a float
 def convert_price(str_price: str):
     float_price = ""
     for c in str_price:
@@ -35,7 +48,7 @@ def convert_price(str_price: str):
         return None
 
 
-#  Returns the HTML of the page at <url>
+# Returns the HTML of the page at <url>
 def get_page(url: str):
     try:
         with closing(get(url)) as response:
@@ -47,7 +60,7 @@ def get_page(url: str):
         print("Error: Could not connect to " + url)
 
 
-#  Returns True if <response> contains HTML
+# Returns True if <response> contains HTML
 def is_valid_response(response) -> bool:
     content_type = response.headers["Content-Type"]
     return (response.status_code == 200 and
